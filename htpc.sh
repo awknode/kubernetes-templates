33ca98887bb4dfb0b4054ade149dbae161e6a517
stack <64115053+awknode@users.noreply.github.com>
2022-01-02 14:29:25 -0700

    Update htpc.sh

--- htpc.sh
+++ htpc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kubectl apply -f namespace.yaml -f plex1.yaml -f pv-volume.yaml -f pvc-volume-claim.yml -f storageclass.yaml -f sonarr.yaml -f rutorrent.yaml -f jackett.yaml -f wireguard.yaml
+kubectl apply -f namespace.yaml -f plex1.yaml -f pv-volume.yaml -f pvc-volume-claim.yml -f storageclass.yaml -f wireguard.yaml -f rutorrent.yaml -f jackett.yaml -f sonarr.yaml -f organizr
 sleep 10s
 kubectl -n plexserver expose deployment/plex --type=LoadBalancer --port 32400
 exit 0;
